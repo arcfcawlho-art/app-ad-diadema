20dddc2d722d1f4d205d9c70f1bfb984782365e8
arcfcawlho-art <arc.fcawlho@gmail.com>
2026-03-08 16:33:26 -0300

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxucjJzhq_JYBgL4d_2WLEsmVu_eKllIs",
-  authDomain: "app-add-diadema.firebaseapp.com",
-  projectId: "app-add-diadema",
-  storageBucket: "app-add-diadema.firebasestorage.app",
+  apiKey: "AIzaSyDxucjJzhq_JYBgL4d_2WLEsmVu_eKllIs", 
+  authDomain: "app-ad-diadema.firebaseapp.com",
+  projectId: "app-ad-diadema",
+  storageBucket: "app-ad-diadema.firebasestorage.app",
   messagingSenderId: "618584991265",
   appId: "1:618584991265:web:f2432bb1268986d7aa2568",
-  databaseURL: "https://app-add-diadema-default-rtdb.firebaseio.com"
+  databaseURL: "https://app-ad-diadema-default-rtdb.firebaseio.com"
 };
 
 const app = initializeApp(firebaseConfig);
